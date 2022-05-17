01bffd6cbec3e36753214a9140745419dcf59f55
Muhammed Ubeyd <muhammedubeyd101112@gmail.com>
2022-05-17 23:37:38 +0300

    adding material header

--- src/containers/Router.tsx
+++ src/containers/Router.tsx
@@ -1,15 +1,22 @@
 // import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
 import { Route, Routes } from "react-router-dom"
 import Login from "../pages/Login"
+import Header from "../components/Header"
 
 export interface Props {}
 
 const RouterComp = ({}: Props) => {
     return (
-        <Routes>
-            <Route path="/" element={<h1>Welcome to Carved Rock Fitness</h1>} />
-            <Route path="/login" element={<Login />} />
-        </Routes>
+        <div>
+            <Header />
+            <main>
+                <Routes>
+                    <Route path="/" element={<h1>Welcome to Office Reservation App</h1>} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="*" element={<h1>Page Not Found 404</h1>} />
+                </Routes>
+            </main>
+        </div>
     )
 }
 
