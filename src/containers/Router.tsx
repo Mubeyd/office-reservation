8ed52b396878e6a0dde4b28eca7a73c3920c729a
Muhammed Ubeyd <muhammedubeyd101112@gmail.com>
2022-05-17 23:58:08 +0300

    inti offices list page

--- src/containers/Router.tsx
+++ src/containers/Router.tsx
@@ -2,6 +2,7 @@
 import { Route, Routes } from "react-router-dom"
 import Login from "../pages/Login"
 import Header from "../components/Header"
+import OfficesList from "../pages/OfficesList"
 
 export interface Props {}
 
@@ -13,6 +14,7 @@ const RouterComp = ({}: Props) => {
                 <Routes>
                     <Route path="/" element={<h1>Welcome to Office Reservation App</h1>} />
                     <Route path="/login" element={<Login />} />
+                    <Route path="/OfficesList" element={<OfficesList />} />
                     <Route path="*" element={<h1>Page Not Found 404</h1>} />
                 </Routes>
             </main>
---
+++ src/pages/OfficesList.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function OfficesList() {
+  return (
+    <div>OfficesList</div>
+  )
+}
+
+
+export default React.memo(OfficesList)
