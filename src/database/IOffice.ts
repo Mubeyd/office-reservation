4d502f8c3c24bfb2e43d84e03ccd87bbe8ca9b93
Muhammed Ubeyd <muhammedubeyd101112@gmail.com>
2022-05-19 20:29:07 +0300

    adding office type

--- src/database/IOffice.ts
+++ src/database/IOffice.ts
@@ -1,6 +1,8 @@
+export type OfficeType = 'personal' | 'team' | 'collaborative'
 export interface IOffice {
     id: number
     name: string
     address: string
     price: number
+    officeType: OfficeType
 }
