e484811c7397b70308c6cf856ec3073308df0966
Muhammed Ubeyd <muhammedubeyd101112@gmail.com>
2022-05-19 22:44:34 +0300

    helper useFetch function

---
+++ src/services/useFetch.ts
@@ -0,0 +1,36 @@
+import { useState, useRef, useEffect } from "react";
+import { baseUrl } from "../constants/constants";
+
+
+export default function useFetch<T>(url:string)  {
+  const isMounted = useRef(false);
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    isMounted.current = true;
+    async function init() {
+      try {
+        const response = await fetch(baseUrl + url);
+        if (response.ok) {
+          const json = await response.json();
+          if (isMounted.current) setData(json);
+        } else {
+          throw response;
+        }
+      } catch (e) {
+        if (isMounted.current) setError(e as any);
+      } finally {
+        if (isMounted.current) setLoading(false);
+      }
+    }
+    init();
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, [url]);
+
+  return { data, error, loading };
+}
