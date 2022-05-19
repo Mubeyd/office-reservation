24e442212cc2ba35d5d55910f6afe143896eac01
Muhammed Ubeyd <muhammedubeyd101112@gmail.com>
2022-05-19 21:17:07 +0300

    editing header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import AdbIcon from "@mui/icons-material/Adb"
-import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
+import MeetingRoomIcon from "@mui/icons-material/MeetingRoom"
 import MenuIcon from "@mui/icons-material/Menu"
 import AppBar from "@mui/material/AppBar"
 import Avatar from "@mui/material/Avatar"
@@ -15,33 +15,54 @@ import Typography from "@mui/material/Typography"
 import * as React from "react"
 import { useCallback } from "react"
 import { useNavigate } from "react-router-dom"
-
-const pages = ["Offices", "Pricing", "Blog"]
-const settings = ["Profile", "Account", "Dashboard", "Logout"]
+import { useUser } from "../hooks/useUser"
 
 const ResponsiveAppBar = () => {
     const navigate = useNavigate()
 
+    const { localUser: user } = useUser()
+
     const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null)
     const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null)
 
-    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    const handleOpenNavMenu = useCallback((event: React.MouseEvent<HTMLElement>) => {
         setAnchorElNav(event.currentTarget)
-    }
-    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    }, [])
+    const handleOpenUserMenu = useCallback((event: React.MouseEvent<HTMLElement>) => {
         setAnchorElUser(event.currentTarget)
-    }
+    }, [])
 
-    const handleCloseNavMenu = () => {
+    const handleCloseNavMenu = useCallback(() => {
         setAnchorElNav(null)
-    }
+    }, [])
 
-    const handleCloseUserMenu = () => {
+    const handleCloseUserMenu = useCallback(() => {
         setAnchorElUser(null)
-    }
+    }, [])
+
+    const onClickProfile = useCallback(() => {
+        setAnchorElUser(null)
+    }, [])
+
+    const onClickLogout = useCallback(() => {
+        localStorage.removeItem("user")
+        navigate("/")
+
+        setAnchorElUser(null)
+    }, [navigate])
+
+    const onClickLogin = useCallback(async () => {
+        navigate("/SignIn")
+
+        setAnchorElUser(null)
+    }, [navigate])
 
     const onNavigateToOfficesList = useCallback(() => {
         navigate("/OfficesList")
+    }, [navigate])
+
+    const onNavigateToReservationsList = useCallback(() => {
+        navigate("/ReservationsList")
     }, [navigate])
 
     return (
@@ -93,7 +114,7 @@ const ResponsiveAppBar = () => {
                                 <Typography textAlign="center">Offices</Typography>
                             </MenuItem>
 
-                            <MenuItem onClick={onNavigateToOfficesList}>
+                            <MenuItem onClick={onNavigateToReservationsList}>
                                 <Typography textAlign="center">Reservations</Typography>
                             </MenuItem>
                         </Menu>
@@ -118,14 +139,13 @@ const ResponsiveAppBar = () => {
                         LOGO
                     </Typography>
                     <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-                        {/* {pages.map((page) => (
-                                ))} */}
-                            <Button onClick={onNavigateToOfficesList} sx={{ my: 2, color: "white", display: "block" }}>
-                                Offices
-                            </Button>
-                            <Button onClick={onNavigateToOfficesList} sx={{ my: 2, color: "white", display: "block" }}>
-                                Reservations
-                            </Button>
+                        <Button onClick={onNavigateToOfficesList} sx={{ my: 2, color: "white", display: "block" }}>
+                            Offices
+                        </Button>
+
+                        <Button onClick={onNavigateToReservationsList} sx={{ my: 2, color: "white", display: "block" }}>
+                            Reservations
+                        </Button>
                     </Box>
 
                     <Box sx={{ flexGrow: 0 }}>
@@ -150,11 +170,23 @@ const ResponsiveAppBar = () => {
                             open={Boolean(anchorElUser)}
                             onClose={handleCloseUserMenu}
                         >
-                            {settings.map((setting) => (
-                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                    <Typography textAlign="center">{setting}</Typography>
+                            {user && (
+                                <MenuItem onClick={onClickProfile}>
+                                    <Typography textAlign="center">Profile</Typography>
                                 </MenuItem>
-                            ))}
+                            )}
+
+                            {user && (
+                                <MenuItem onClick={onClickLogout}>
+                                    <Typography textAlign="center">Logout</Typography>
+                                </MenuItem>
+                            )}
+
+                            {!user && (
+                                <MenuItem onClick={onClickLogin}>
+                                    <Typography textAlign="center">Login</Typography>
+                                </MenuItem>
+                            )}
                         </Menu>
                     </Box>
                 </Toolbar>
